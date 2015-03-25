0abf6eb1526e0ac611f766c3d77ad6266515e679
testgithub1245 <testgithub1245@gmail.com>
2015-03-25 21:38:05 +0000

    change footer view model

---
+++ PrehistoriaWebsite.WebUI/ViewModels/Admin/FooterModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PrehistoryWebsite.Domain;
+
+namespace PrehistoryWebsite.Models.Admin
+{
+    public class FooterModel
+    {
+        public int Id { get; set; }
+        public string title1 { get; set; }
+        public string title2 { get; set; }
+        public string title3 { get; set; }
+        [AllowHtml]
+        public string content1 { get; set; }
+        [AllowHtml]
+        public string content2 { get; set; }
+
+        public void SetFooter(Footer footer)
+        {
+            this.Id = footer.Id;
+            this.title1 = footer.title1;
+            this.title2 = footer.title2;
+            this.title3 = footer.title3;
+            this.content1 = footer.content1;
+            this.content2 = footer.content2;
+        }
+
+		// get domain footer model from view footer model
+        public Footer GetFooter()
+        {
+            Footer footer = new Footer
+            {
+                Id = this.Id,
+                title1 = this.title1,
+                title2 = this.title2,
+                title3 = this.title3,
+                content1 = this.content1,
+                content2 = this.content2
+            };
+
+            return footer;
+        }
+    }
+}
